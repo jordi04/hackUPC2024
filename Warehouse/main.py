585b97154f4b3f858629af2e8183cb21445b2ad9
jordiroca <jordipauroca@gmail.com>
2024-05-05 08:54:26 +0200

    Final

--- Warehouse/main.py
+++ Warehouse/main.py
@@ -1,8 +1,9 @@
 import speech_recognition as sr
 import csv
 from nltk.tokenize import word_tokenize
-
+import numpy as np
 import json
+import sys
 
 print("Welcome! You are talking to Seidor's warehouse shopping list AI voice assistant.")
 print("\n")
@@ -16,27 +17,40 @@ print("\n")
 print("You can confirm the shopping list by saying 'confirm'.")
 print("If you want to cancel the shopping list, just say 'cancel'.")
 print("\n")
+print("Devoleped by Jaume Mora, Carlos Lopez, Sergi Adrover and Jordi Roca.")
+print("\n")
 
 
 list_id = 0
 r = sr.Recognizer()
-words_in_csv = set()
 
 def initialize_json(filename='shoppingList.json'):
     initial_data = {"products": []}
-    print("Initializing JSON file..." + filename)
+    #print("Initializing JSON file..." + filename)
     with open(filename, 'w') as file:
         json.dump(initial_data, file)
 
 # Call the function to initialize the JSON file
 initialize_json()
 
+words_in_csv = set()
 with open('products.csv', 'r') as f:
     reader = csv.reader(f, delimiter=';')
     next(reader)  # Skip the header
     for row in reader:
         words_in_csv.update(word_tokenize(row[1].lower()))  # Convert to lowercase for case-insensitive comparison
 
+words_in_csv_easy = set()
+quantity_in_csv = np.array([])
+with open('productsCopy.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=';')
+    next(reader)  # Skip the header
+    for row in reader:
+        words_in_csv_easy.update(word_tokenize(row[0].lower()))
+        quantity_in_csv = np.append(quantity_in_csv, row[1])
+
+#print (words_in_csv_easy, "noonon")
+#print(quantity_in_csv, "yeeys")
 def save_csv(data, filepath):
     with open(filepath, 'w', newline='') as f:
         writer = csv.writer(f)
@@ -49,6 +63,7 @@ def record_text():
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
+            #textAux = find_closest_match(text.lower(), words_in_csv_easy)
             #print(f"I understood: {text}")
             return text.lower()
         
@@ -56,14 +71,12 @@ def record_text():
             print("Sorry I didn't get that, please try again.")
             return None
         
-
-
 def add_to_list(text, quantity):
     try:
         # Load existing data from file
         with open('shoppingList.json', 'r') as file:
             data = json.load(file)
-        print("data",data,data['products'])
+        #print("data",data,data['products'])
         
         # Add new item to the products
         data['products'].append({"name": text, "amount": quantity})
@@ -73,7 +86,6 @@ def add_to_list(text, quantity):
             json.dump(data, file, indent=4)  # indent for better readability of the JSON file
         
         print(f"Added {quantity} {text} to the shopping list.")
-        
 
     except FileNotFoundError:
         print("The file was not found.")
@@ -81,6 +93,7 @@ def add_to_list(text, quantity):
         print("Error decoding JSON.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
 def print_json():
     with open('shoppingList.json', 'r') as file:
         data = json.load(file)
@@ -121,8 +134,9 @@ def interpret_text(text):
         if (len(auxText) > 1):
             what_to_add = find_closest_match(auxText[1], words_in_csv)
             if (check_substring(text, 'units')):
-                print(f"Adding: {what_to_add}")
-                add_to_list(what_to_add, detect_ammount(auxText[1]))
+                ammount = detect_ammount(auxText[1])
+                print(f"Adding: {ammount} units of {what_to_add}")
+                add_to_list(what_to_add, ammount)
                 
             else:
                 print(f"How many {what_to_add} do you want to add?")
@@ -146,14 +160,23 @@ def interpret_text(text):
     elif (check_substring(text, 'confirm')):
         print("Your shopping list:")
         print_json()
+        print("Want to confirm?")
         confirmation = record_text()
-        if confirmation == 'yes' or confirmation == 'confirm' or confirmation == 'correct':
-            print("Saving shopping list...")
-            shopping_list = [[line.strip()] for line in lines]
-            save_csv(shopping_list, 'shopping_list.csv')
-            not_confirmed = False
-        else:
-            print("Shopping list not saved, you can continue modifying it.")
+        entering = True
+        while entering:
+            if check_substring(confirmation, 'yes') or check_substring(confirmation, 'confirm') or check_substring(confirmation, 'correct') or check_substring(confirmation, 'ok'):
+                print("Saving shopping list...")
+                print("...")
+                print("Shopping list saved successfully. Have a nice day!")
+                entering = False
+                confirmed = True
+                sys.exit()
+            elif check_substring(confirmation, 'no') or check_substring(confirmation, 'cancel') or confirmation == 'wrong':
+                print("Shopping list not saved, you can continue modifying it.")
+                entering = False
+            else:
+                print("I'm sorry, I didn't understand that. Please try again.")
+                confirmation = record_text()
     else:
         print("I'm sorry, I didn't understand that. Please try again.")
 
@@ -192,7 +215,7 @@ def find_closest_match(text, words):
             min_distance = distance
             closest_match = entry
     return closest_match
-not_confirmed = True
-while not_confirmed:
+confirmed = False
+while not confirmed:
     interpret_text(record_text())
-    
+
